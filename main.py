29af2a36ec9425fb3d6b4c5adadcda845761afab
lordcowie <mootezjebalia@gmail.com>
2024-06-09 20:20:58 -0700

    cleaning code and adding some documentation

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ import asyncio
 from kucoin_apis import KuCoinAPI
 import logging
 
+#INFO:root:Received message: {'id': 's13u6l50tc', 'type': 'welcome'} welcome message when websocket connection is successfull
+#INFO:root:Received message: {'id': '1717989479', 'type': 'ack'} ack message when topic subscription is successfull
+
+
 
 load_dotenv() # take environment variables from .env
 #hiding sensitive info for now in case I make this project public in the future 
@@ -13,10 +17,10 @@ api_secret = os.getenv('API_SECRET')
 api_passphrase = os.getenv('API_PASSPHRASE')
 
 async def main():
-    load_dotenv()
     api = KuCoinAPI()
-     # Check if API credentials are loaded
-    logging.info(f"API Key: {api.api_key}, Secret: {api.api_secret}")
     while True:
         await start_kucoin_websocket(api)
         await asyncio.sleep(10) # wait before attempting to reconnect 
+
+if __name__ == "__main__":
+    asyncio.run(main())        
