1e8e527bbd75558d59c3bcdcda2a4411306cb846
lordcowie <mootezjebalia@gmail.com>
2024-06-09 18:38:51 -0700

    added .env file for api key security

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from websocket_handler import start_websocket
+from dotenv import load_dotenv
+import os 
 
+load_dotenv() # take environment variables from .env
 #hiding sensitive info for now in case I make this project public in the future 
-api_key = ""
-api_secret = ""
-api_passphrase = ""
+api_key = os.getenv('API_KEY')
+api_secret = os.getenv('API_SECRET')
+api_passphrase = os.getenv('API_PASSPHRASE')
 
 start_websocket(api_key, api_secret, api_passphrase)
